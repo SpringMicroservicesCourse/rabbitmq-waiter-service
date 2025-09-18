b0ca87c2811b09a455f32391c51a170a5f889fe1
chukuolung <chukuo99@gmail.com>
2025-09-18 09:45:50 +0800

    修復 StreamBridge 發送目標名稱
    - 將 Customer.NOTIFY_ORDERS 改為 'notifyOrders-out-0'
    - 確保與 application.properties 中的綁定名稱匹配
    - 修復消息無法正確發送給 Customer Service 的問題

--- src/main/java/tw/fengqing/spring/springbucks/waiter/integration/OrderListener.java
+++ src/main/java/tw/fengqing/spring/springbucks/waiter/integration/OrderListener.java
@@ -1,7 +1,11 @@
 package tw.fengqing.spring.springbucks.waiter.integration;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Consumer;
@@ -14,15 +18,25 @@ import java.util.function.Consumer;
 @Slf4j
 public class OrderListener {
 
+    @Autowired
+    private StreamBridge streamBridge;
+
     /**
      * 處理完成訂單的函數式 Bean
-     * 接收完成訂單 ID 並記錄日誌
+     * 接收完成訂單 ID 並通知客戶
      * @return 完成訂單處理函數
      */
     @Bean
     public Consumer<Long> finishedOrders() {
         return id -> {
             log.info("We've finished an order [{}].", id);
+            
+            // 發送通知給客戶
+            Message<Long> message = MessageBuilder.withPayload(id)
+                    .setHeader("customer", "Li Lei")
+                    .build();
+            log.info("Notify the customer: Li Lei");
+            streamBridge.send("notifyOrders-out-0", message);
         };
     }
 }
